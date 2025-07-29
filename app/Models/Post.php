7476fcee18628d409663901a23efff4c0c773f83
Reyn12 <muhammadrenaldi687@gmail.com>
2025-07-29 20:34:16 +0700

    Add Laravel project with database migrations

---
+++ app/Models/Post.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'category_id',
+        'location_name',
+        'photo_url',
+        'status',
+        'rejection_reason',
+        'likes_count',
+        'comments_count',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'post_id');
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class, 'post_id');
+    }
+}
